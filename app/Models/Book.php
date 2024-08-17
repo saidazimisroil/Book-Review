fa0b82b4250e36561ed86faa709b25c5a4c93287
Saidazim <134857217+saidazimisroil@users.noreply.github.com>
2024-08-18 00:44:43 +0500

    add local query scope

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,4 +14,9 @@ class Book extends Model
     {
         return $this->hasMany(Review::class);
     }
+
+    public function scopeTitle(Builder $query, string $title) : Builder
+    {
+        return $query->where("title","like","%". $title ."%");
+    } 
 }
