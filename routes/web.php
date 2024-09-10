bd92d6366760fbbeb4d4eccaa10c14aaa2e7cd33
Saidazim <saidazim.s.0204@gmail.com>
2024-09-10 14:20:43 +0500

    add creating review

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('books.index');
 });
 
-Route::resource('books', BookController::class);
+Route::resource('books', BookController::class)
+    ->only(['index','show']);
+
+Route::resource('books.reviews', ReviewsController::class)
+    ->only(['create', 'store']);
